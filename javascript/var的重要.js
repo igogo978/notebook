4880b2e58dea011c0f23e5126c36d8ef33e5e2c3
igogo978 <chia0210@gmail.com>
2016-01-27 18:29:23 +0800

    全域變數

--- javascript/var的重要.js
+++ javascript/var的重要.js
@@ -5,11 +5,9 @@
 		var sum1 ;
 		sum1 = x+y;	
 		sum2 = x+y ;
-		
-		
 	}
 	add(1,2);
-	console.log(sum2);
-	console.log(sum1);
+	console.log(sum2);  //結果等於3, 自動成為一個全域變數
+	console.log(sum1); //警告訊息, not defined
 </script>
 
